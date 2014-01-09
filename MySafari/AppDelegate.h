c039b00e7d64f4629a917600adc5b9090c3e8df2
mjvoracek <mjvoracek@gmail.com>
2014-01-09 13:38:38 -0600

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Matthew Voracek on 1/9/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MySafari/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Matthew Voracek on 1/9/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
